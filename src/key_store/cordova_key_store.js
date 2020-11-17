2fc39011d39f83f733c142edc47c5791bd5e6d08
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2020-11-18 00:55:00 +0900

    Add Wallet#keys method

--- src/key_store/cordova_key_store.js
+++ src/key_store/cordova_key_store.js
@@ -28,7 +28,11 @@ var __awaiter =
     });
   };
 Object.defineProperty(exports, '__esModule', { value: true });
+const tapyrus = require('tapyrusjs-lib');
 class CordovaKeyStore {
+  constructor(network) {
+    this.network = network;
+  }
   addPrivateKey(wif) {
     this.get('tapyrus/wallet/key/count').then(count => {
       this.set(`tapyrus/wallet/key/${count}`, wif);
@@ -39,6 +43,40 @@ class CordovaKeyStore {
     this.get('tapyrus/wallet/ext/count').then(count => {
       this.set(`tapyrus/wallet/ext/${count}`, extendedPrivateKey);
       this.set(`tapyrus/wallet/ext/count`, (count + 1).toString());
+    });
+  }
+  keys() {
+    return __awaiter(this, void 0, void 0, function*() {
+      const privKeys = yield this.get('tapyrus/wallet/key/count').then(value =>
+        __awaiter(this, void 0, void 0, function*() {
+          const count = Number(value);
+          const keys = [];
+          for (let i = 0; i < count; i++) {
+            const wif = yield this.get(`tapyrus/wallet/key/${i}`);
+            const key = tapyrus.ECPair.fromWIF(
+              wif,
+              this.network,
+            ).privateKey.toString('hex');
+            keys.push(key);
+          }
+          return keys;
+        }),
+      );
+      const extKeys = yield this.get('tapyrus/wallet/ext/count').then(value =>
+        __awaiter(this, void 0, void 0, function*() {
+          const count = Number(value);
+          const keys = [];
+          for (let i = 0; i < count; i++) {
+            const xpriv = yield this.get(`tapyrus/wallet/ext/${i}`);
+            const key = tapyrus.bip32
+              .fromBase58(xpriv, this.network)
+              .privateKey.toString('hex');
+            keys.push(key);
+          }
+          return keys;
+        }),
+      );
+      return privKeys.concat(extKeys);
     });
   }
   get(key) {
--- ts_src/key_store/cordova_key_store.ts
+++ ts_src/key_store/cordova_key_store.ts
@@ -1,8 +1,15 @@
+import * as tapyrus from 'tapyrusjs-lib';
 import { KeyStore } from '../key_store';
 
 declare let cordova: any;
 
 export class CordovaKeyStore implements KeyStore {
+  private network: tapyrus.networks.Network;
+
+  constructor(network: tapyrus.networks.Network) {
+    this.network = network;
+  }
+
   addPrivateKey(wif: string): void {
     this.get('tapyrus/wallet/key/count').then(count => {
       this.set(`tapyrus/wallet/key/${count}`, wif);
@@ -15,6 +22,40 @@ export class CordovaKeyStore implements KeyStore {
       this.set(`tapyrus/wallet/ext/${count}`, extendedPrivateKey);
       this.set(`tapyrus/wallet/ext/count`, (count + 1).toString());
     });
+  }
+
+  async keys(): Promise<string[]> {
+    const privKeys = await this.get('tapyrus/wallet/key/count').then(
+      async value => {
+        const count = Number(value);
+        const keys: string[] = [];
+        for (let i = 0; i < count; i++) {
+          const wif = await this.get(`tapyrus/wallet/key/${i}`);
+          const key = tapyrus.ECPair.fromWIF(
+            wif,
+            this.network,
+          ).privateKey!.toString('hex');
+          keys.push(key);
+        }
+        return keys;
+      },
+    );
+    const extKeys = await this.get('tapyrus/wallet/ext/count').then(
+      async value => {
+        const count = Number(value);
+        const keys: string[] = [];
+
+        for (let i = 0; i < count; i++) {
+          const xpriv = await this.get(`tapyrus/wallet/ext/${i}`);
+          const key = tapyrus.bip32
+            .fromBase58(xpriv, this.network)
+            .privateKey!.toString('hex');
+          keys.push(key);
+        }
+        return keys;
+      },
+    );
+    return privKeys.concat(extKeys);
   }
 
   private async get(key: string): Promise<string> {
